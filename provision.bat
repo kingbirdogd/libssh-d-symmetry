e81ef74a4b459a6231480bca44ff33d15a6d4043
David Au <dau@symmetryinvestments.com>
2021-10-17 16:04:08 +0100

    fixed windowns compile

--- provision.bat
+++ provision.bat
@@ -6,5 +6,8 @@ set build_dir=%source_dir%build\%platform%
 
 git clone -q https://github.com/microsoft/vcpkg %source_dir%\vcpkg
 call %source_dir%\vcpkg\bootstrap-vcpkg.bat
+%source_dir%\vcpkg\vcpkg.exe install openssl:x64-windows-static
 %source_dir%\vcpkg\vcpkg.exe install libssh:x64-windows-static
+%source_dir%\vcpkg\vcpkg.exe install openssl:x86-windows-static
 %source_dir%\vcpkg\vcpkg.exe install libssh:x86-windows-static
+
