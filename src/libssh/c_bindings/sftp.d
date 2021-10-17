8c40868a03eaa8c9a47e2a99d5e127b984f3d614
David Au <dau@symmetryinvestments.com>
2021-10-18 00:32:23 +0100

    use the correct file mode file

--- src/libssh/c_bindings/sftp.d
+++ src/libssh/c_bindings/sftp.d
@@ -3,40 +3,31 @@
 import libssh.c_bindings.libssh;
 import libssh.c_bindings.ctypes;
 
-enum O_ACCMODE      = 0x3;
-enum O_RDONLY       = 0x0;
-enum O_WRONLY       = 0x1;
-enum O_RDWR         = 0x2;
+version(Windows)
+{
+	public import core.stdc.stdio;
+    enum S_IRUSR    = 0x100;  // octal 0400
+    enum S_IWUSR    = 0x080;  // octal 0200
+    enum S_IXUSR    = 0x040;  // octal 0100
+    enum S_IRWXU    = S_IRUSR | S_IWUSR | S_IXUSR;
 
-enum O_CREAT        = 0x40;     // octal     0100
-enum O_EXCL         = 0x80;     // octal     0200
-enum O_NOCTTY       = 0x100;    // octal     0400
-enum O_TRUNC        = 0x200;    // octal    01000
+    enum S_IRGRP    = S_IRUSR >> 3;
+    enum S_IWGRP    = S_IWUSR >> 3;
+    enum S_IXGRP    = S_IXUSR >> 3;
+    enum S_IRWXG    = S_IRWXU >> 3;
 
-enum O_APPEND       = 0x400;    // octal    02000
-enum O_NONBLOCK     = 0x800;    // octal    04000
-enum O_CLOEXEC      = 0x80000;  // octal 02000000
-enum O_SYNC         = 0x101000; // octal 04010000
-enum O_DSYNC        = 0x1000;   // octal   010000
-enum O_RSYNC        = O_SYNC;
+    enum S_IROTH    = S_IRGRP >> 3;
+    enum S_IWOTH    = S_IWGRP >> 3;
+    enum S_IXOTH    = S_IXGRP >> 3;
+    enum S_IRWXO    = S_IRWXG >> 3;
 
-enum S_IRUSR    = 0x100;  // octal 0400
-enum S_IWUSR    = 0x080;  // octal 0200
-enum S_IXUSR    = 0x040;  // octal 0100
-enum S_IRWXU    = S_IRUSR | S_IWUSR | S_IXUSR;
-
-enum S_IRGRP    = S_IRUSR >> 3;
-enum S_IWGRP    = S_IWUSR >> 3;
-enum S_IXGRP    = S_IXUSR >> 3;
-enum S_IRWXG    = S_IRWXU >> 3;
-
-enum S_IROTH    = S_IRGRP >> 3;
-enum S_IWOTH    = S_IWGRP >> 3;
-enum S_IXOTH    = S_IXGRP >> 3;
-enum S_IRWXO    = S_IRWXG >> 3;
-
-enum S_ISUID    = 0x800; // octal 04000
-enum S_ISGID    = 0x400; // octal 02000
+    enum S_ISUID    = 0x800; // octal 04000
+    enum S_ISGID    = 0x400; // octal 02000
+}
+else
+{
+	public import core.sys.posix.fcntl;
+}
 
 enum auto LIBSFTP_VERSION = 3;
 
